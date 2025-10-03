58027861ddf81ace862673944be70403acd19cb1
oscar.sanchez <oscar.sanchez@savne.net>
2025-10-02 19:38:10 -0500

    Ejercicios para entender el uso de las secuencias en rust

--- maldelbrot/src/main.rs
+++ maldelbrot/src/main.rs
@@ -1,8 +1,55 @@
 use num::Complex;
+use std::str::FromStr;
 
-fn main() {
-    println!("Hello, world!");
+/// Parse the string `s` as a coordinate pais, like `"400x600"` or `"0.0,0.5"`.
+/// 
+/// Specifically, `s` should have the form <left><sep><right>, where <sep> is 
+/// the character given by the `separator` argument, and <left> and <right> are
+/// both string that can be parsed by `T::from_str`. `separator` must be aun
+/// ASCII character.
+/// 
+/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
+/// correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r))=> Some((l ,r)),
+                _ => None
+            }
+        }
+    }
 }
+
+fn main(){
+    let text = "Hello, Rust!";
+    let index_of_r = text.find('R');
+    let index_of_r= index_of_r.unwrap();
+    
+    for i in 0..=index_of_r {
+        print!("{} ", i);
+        println!("{}", text.chars().nth(i).unwrap());
+    }
+    println!("--{}", text.len());
+    
+    for i in (index_of_r+1)..text.len(){
+        print!("{} ", i);
+        println!("{}", text.chars().nth(i).unwrap());
+    }
+    
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>(" ", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    
+}
+
+
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
@@ -12,13 +59,6 @@ fn main() {
 /// origin. If `c` seems to be a member (more precisely, if we reached the 
 /// iteration limit without being able to prove that `c` is not a member),
 /// retutn `None`.
-fn complex_square_add_loop(mut c: Complex<f64>) {
-    let mut z = Complex {re:0.0, im: 0.0};
-    loop{
-        z = z * z + c;
-    }
-}
-
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0};
     for i in 0..limit {
