0691ea82bf79ed64ca1493dc794ad235c42ffa73
oscar.sanchez <oscar.sanchez@savne.net>
2025-10-03 17:48:02 -0500

    Se crea función parse_complex para convertir coordenadas a un número complejo

--- maldelbrot/src/main.rs
+++ maldelbrot/src/main.rs
@@ -14,7 +14,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
                 (Ok(l), Ok(r))=> Some((l ,r)),
                 _ => None
             }
@@ -22,23 +22,6 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
-fn main(){
-    let text = "Hello, Rust!";
-    let index_of_r = text.find('R');
-    let index_of_r= index_of_r.unwrap();
-    
-    for i in 0..=index_of_r {
-        print!("{} ", i);
-        println!("{}", text.chars().nth(i).unwrap());
-    }
-    println!("--{}", text.len());
-    
-    for i in (index_of_r+1)..text.len(){
-        print!("{} ", i);
-        println!("{}", text.chars().nth(i).unwrap());
-    }
-    
-}
 
 #[test]
 fn test_parse_pair() {
@@ -46,15 +29,35 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,", ','), None);
     assert_eq!(parse_pair::<i32>(",10", ','), None);
     assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
     
 }
 
+/// Parse a pair of floating-point number separated by a comma as a complex
+// number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"), 
+        Some(Complex { re: 1.25, im: -0.0625})
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
 
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
 ///
-/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// If `c` is not a member, return `Some(i))`, where `i` is the number of
 /// iterations it took for `c` to leave the circle of radius 2 centered on the 
 /// origin. If `c` seems to be a member (more precisely, if we reached the 
 /// iteration limit without being able to prove that `c` is not a member),
