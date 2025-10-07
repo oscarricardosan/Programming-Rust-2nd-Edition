2c586b27da48c4aac1b8f18686754c17711781f3
oscar.sanchez <oscar.sanchez@savne.net>
2025-10-07 18:34:13 -0500

    Generación de gráfica Maldelbrot usando un solo núcleo del procesador

--- maldelbrot/src/main.rs
+++ maldelbrot/src/main.rs
@@ -1,5 +1,36 @@
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder; 
+use std::fs::File;
+use std::env;
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+    
+    let bounds= parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+        
+    let upper_left= parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+        
+    let lower_right= parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+    
+    let mut pixels= vec![0; bounds.0 * bounds.1];
+    
+    render(&mut pixels, bounds, upper_left, lower_right);
+    
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writting PNG file");
+        
+}
 
 /// Parse the string `s` as a coordinate pais, like `"400x600"` or `"0.0,0.5"`.
 /// 
@@ -53,6 +84,69 @@ fn test_parse_complex() {
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+/// 
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicaing a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the area our image covers.
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>
+) -> Complex<f64> {
+    
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im
+    );
+    
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+    // Why substraction here? pixel.1 increases as we go down,
+    // but the imaginary component increases as we go up.
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200), (25, 175), Complex{ re: -1.0, im: 1.0}, Complex{ re: 1.0, im: -1.0}
+        ),
+        Complex { re: -0.5, im: -0.75}
+    );
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixeles.
+/// 
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// wich holds one grayscale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on the complex plane corresponding to the upper-
+/// left and lower-right corners of the pixel buffer.
+fn render (
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>
+){
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(
+                bounds, (column, row), upper_left, lower_right
+            );
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None=> 0,
+                Some(count)=> 255 - count as u8
+            }
+        }
+    }
+}
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
@@ -71,4 +165,22 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
         z = z * z + c;
     }
     None
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file named `filename`.
+fn write_image(
+    filename: &str, pixels: &[u8], bounds: (usize, usize)
+) -> Result<(), std::io::Error> {
+    let output= File::create(filename)?;
+    
+    let enconder= PNGEncoder::new(output);
+    enconder.encode(
+      &pixels,
+      bounds.0 as u32,  
+      bounds.1 as u32,
+      ColorType::Gray(8)
+    )?;
+    
+    Ok(())
 }
