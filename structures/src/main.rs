ee34472e3f1d3815dfd4534ff149c9bfe740639c
oscar.sanchez <oscar.sanchez@savne.net>
2025-12-29 17:29:36 -0500

    Ejemplo de implementación en una estructura. Pág 371

---
+++ structures/src/main.rs
@@ -0,0 +1,46 @@
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+            println!("Reasigno jobs");
+            std::mem::swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+        self.older.pop()
+    }
+}
+
+fn main() {
+    let mut q = Queue {
+        older: Vec::new(),
+        younger: Vec::new(),
+    };
+
+    q.push('0');
+    q.push('1');
+
+    //Lee la cola de older, esta vacía, voltea younger en older
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('8');
+    assert_eq!(q.pop(), Some('1'));
+
+    //Lee la cola de older, esta vacía por los dos anteriores pop, luego voltea younger en older
+    assert_eq!(q.pop(), Some('8'));
+
+    //Lee la cola de older, esta vacía al igual que younger, retorna None
+    assert_eq!(q.pop(), None);
+
+    println!("Finalización exitosa");
+}
