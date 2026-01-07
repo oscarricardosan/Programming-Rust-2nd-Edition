e1e12894ffc74e0e708bdb83df48ef5e2f2d82d0
oscar.sanchez <oscar.sanchez@savne.net>
2026-01-06 19:57:38 -0500

    Ejemplo de manejo de enumeraciones con valores

---
+++ enums/src/bin/with-data.rs
@@ -0,0 +1,76 @@
+#[derive(Debug, PartialEq)]
+#[allow(dead_code)]
+enum TimeUnit {
+    Days,
+    Months,
+    Years,
+}
+
+#[allow(dead_code)]
+impl TimeUnit {
+    fn plural(self) -> &'static str {
+        match self {
+            TimeUnit::Days => "days",
+            TimeUnit::Months => "months",
+            TimeUnit::Years => "years",
+        }
+    }
+
+    fn singular(self) -> &'static str {
+        self.plural().trim_end_matches('s')
+    }
+}
+
+#[derive(Debug, PartialEq)]
+#[allow(dead_code)]
+enum RoughtTime {
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+
+//Enum con campos nombrados
+#[derive(Debug)]
+enum Point3d {
+    Origin,
+}
+#[allow(dead_code)]
+enum Shape {
+    Sphere { center: Point3d, radius: f32 },
+    Cuboid { corner1: Point3d, corner2: Point3d },
+}
+
+//Enum con los tres tipos
+// Sin datos [Single...]
+// tupla[ItsComplicated(Option<String>)]
+// struct [ItsExtremelyComplicated { car: Point3d, cdr: Point3d }]
+#[allow(dead_code)]
+enum RelationshipStatus {
+    Single,
+    InARelationship,
+    ItsComplicated(Option<String>),
+    ItsExtremelyComplicated { car: Point3d, cdr: Point3d },
+}
+
+fn main() {
+    let seven_yers = RoughtTime::InTheFuture(TimeUnit::Years, 7);
+    dbg!(&seven_yers);
+
+    let years: Option<TimeUnit> = match seven_yers {
+        RoughtTime::InTheFuture(years, _) => Some(years),
+        _ => None,
+    };
+    assert_eq!(years.unwrap().plural(), "years");
+
+    let unit_sphere = Shape::Sphere {
+        center: Point3d::Origin,
+        radius: 1.0,
+    };
+    let center: Option<Point3d> = match unit_sphere {
+        Shape::Sphere { center, radius: _ } => Some(center),
+        _ => None,
+    };
+
+    dbg!(center);
+    println!("✅ Finalizado!");
+}
