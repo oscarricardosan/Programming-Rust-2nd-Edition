25806212dada273545f8d5f78daf4826927d8370
oscar.sanchez <oscar.sanchez@savne.net>
2026-01-26 09:12:15 -0500

    Ejemplos extra de funciones genericas con traits.

--- traits/src/bin/trait.rs
+++ traits/src/bin/trait.rs
@@ -1,5 +1,7 @@
 use std::io::Write;
 use std::fs::File;
+use std::hash::Hash;
+use std::fmt::Debug;
 
 fn main() -> std::io::Result<()> {
     let mut local_file= File::create("hello.txt")?;
@@ -31,3 +33,23 @@ fn say_hello_generic_fun<W: Write>(out: &mut W) -> std::io::Result<()> {
     out.write_all(b"hello world\n")?;
     out.flush()
 }
+
+// Un parametro que tenga el trait Debug para ser imprimible
+// Eq para operaciones de comparación
+// Hash opara creación de tablas
+// u32 y String implementan los
+// fn top_ten<T: Debug + Hash + Eq>(values: &Vec<T>)
+
+//Función generica con múltiples parámetros
+// fn run_query<M: Mapper + Serialize, R: Reducer + Serialize>(data: &DataSet, map: M, reduce: e)->Results
+
+//Función generica con múltiples parámetros, usando el where
+// para abreviar los trait que deben implementan los parametros
+// fn run_query<M, R>(data: &DataSet, map: M, reduce: e)->Results
+//      where M: Mapper + Serialize,
+//            R: Reducer + Serialize {}
+
+// Función generica con timelimes
+// fn nearest<´t, 'c, P>(target: &'t P, candidates: &'c [P])-> &'c P
+//    where P:MeasureDistance
+//    {...}
