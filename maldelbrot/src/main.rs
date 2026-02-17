d29cb49b7306041b0ed982015c3dab33ab53ddbe
oscar.sanchez <oscar.sanchez@savne.net>
2026-02-17 17:50:24 -0500

    Implementación de Rayon para realizar el balanceo de cargas en paralelo

--- maldelbrot/src/main.rs
+++ maldelbrot/src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::str::FromStr;
 use image::ColorType;
 use image::png::PNGEncoder; 
@@ -25,34 +26,29 @@ fn main() {
     
     let mut pixels= vec![0; bounds.0 * bounds.1];
     
-    let threads= 8;
-    let rows_per_band = bounds.1 / threads + 1;
-    
     {
-        let bands: Vec<&mut [u8]>= pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        let bands: Vec<(usize, &mut [u8])>= pixels
+            .chunks_mut(bounds.0)
+            .enumerate()
+            .collect();
         
-        crossbeam::scope(|spawner| {
-            for (i, band) in  bands.into_iter().enumerate() {
-                
-                let top= rows_per_band * i;
-                let height= band.len() / bounds.0;
-                let band_bounds= (bounds.0, height);
+        bands.into_par_iter()
+            .for_each(|(i, band)|{
+
+                let top= i;
+                let band_bounds= (bounds.0, 1);
                 
                 let band_upper_left = pixel_to_point(
                     bounds, (0, top), upper_left, lower_right
                 );
                 let band_lower_right = pixel_to_point(
-                    bounds, (bounds.0, top + height), upper_left, lower_right
+                    bounds, (bounds.0, top + 1), upper_left, lower_right
                 );
-                
-                spawner.spawn(move |_| {
-                    render(
-                        band, band_bounds, band_upper_left, band_lower_right
-                    );
-                });
-                
-            }
-        }).unwrap();
+
+                render(
+                    band, band_bounds, band_upper_left, band_lower_right
+                );
+            });
         
     }
     
